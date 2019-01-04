8774a7d115a1ffa139851a7396fef71537af898d
Jacques Nyilinkindi <jacknyilinkindi@gmail.com>
2019-01-04 05:17:28 +0200

    ft #162884128 Created Meetup API with tests

---
+++ test/app.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const {expect} = require('chai');
+
+const app = require('../app');
+
+const fixtures = require('./fixtures');
+
+describe('Questioner API Tests', () => {
+
+    it('Adding new meetup...', (done) => {
+        request(app)
+        .post('/api/v1/meetups')
+        .send(fixtures.meetup_post)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+            expect(res.body).to.be.a('object');
+            expect(res.body.status).to.be.equal(200);
+            done();
+        });
+    });
+});
---
+++ test/fixtures.js
@@ -0,0 +1,15 @@
+const meetup_post = {
+    "location": "Telecom House, Kigali",
+    "images": [
+        "https://jnkindi.github.io/Questioner/UI/images/slider-1.png",
+        "https://jnkindi.github.io/Questioner/UI/images/slider-2.png",
+        "https://jnkindi.github.io/Questioner/UI/images/slider-2.png"],
+    "topic": "Fellowship Talks",
+    "description": "We shall talk about everything on fellowship and how to get started",
+    "happeningOn": "01-01-2019",
+    "tags": ["Andela", "Fellowship", "jgjj"]
+};
+
+module.exports = {
+    meetup_post
+}
