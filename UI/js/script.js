7fff528d7d7949e46a507dd2c795c98d82f5b461
Jacques Nyilinkindi <jacknyilinkindi@gmail.com>
2019-02-25 22:16:42 +0200

    ft #163182305 JS Airbnb standards

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,61 +1,61 @@
-if(document.getElementById("js-navbar-toggle")) {
-	let mainNav = document.getElementById("js-menu");
-	let navBarToggle = document.getElementById("js-navbar-toggle");
-	navBarToggle.addEventListener("click", function() {
-	mainNav.classList.toggle("active");
-	});
+
+if (document.getElementById('js-navbar-toggle')) {
+    const mainNav = document.getElementById('js-menu');
+    const navBarToggle = document.getElementById('js-navbar-toggle');
+    navBarToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('active');
+    });
 }
 let clicknbr = 0;
-function nextimage(){
-	clicknbr++;
-	let image = "";
-	if (clicknbr % 3 == 0) {
-		image = "slider-1.png";
-	}
-	if (clicknbr % 3 == 1) {
-		image = "slider-2.png";
-	}
-	if (clicknbr % 3 == 2) {
-		image = "slider-3.png";
-	}
-	let section = document.getElementsByClassName('header-home-transparent');
-	section[0].removeAttribute("href");
-	section[0].setAttribute("style", "background-image: url('../images/"+image+"')");
+function nextimage() {
+    clicknbr += 1;
+    let image = '';
+    if (clicknbr % 3 === 0) {
+        image = 'slider-1.png';
+    }
+    if (clicknbr % 3 === 1) {
+        image = 'slider-2.png';
+    }
+    if (clicknbr % 3 === 2) {
+        image = 'slider-3.png';
+    }
+    const section = document.getElementsByClassName('header-home-transparent');
+    section[0].removeAttribute('href');
+    section[0].setAttribute('style', `background-image: url('../images/${image}')`);
 }
 
-function previousimage(){
-	clicknbr++;
-	nextimage();
+function previousimage() {
+    clicknbr += 1;
+    nextimage();
 }
 
+let slideIndex = 1;
+if (document.getElementsByClassName('slide').length !== 0) {
+    slideIndex = 1;
+    showSlides(slideIndex);
 
-
-if(document.getElementsByClassName("slide").length != 0){
-	var slideIndex = 1;
-	showSlides(slideIndex);
-
-	setInterval(
-		function(){
-			plusSlides(1);
-		},
-	5000);
+    setInterval(
+        () => {
+            plusSlides(1);
+    }, 5000,
+    );
 }
 
 function plusSlides(n) {
-	showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function showSlides(n) {
-	var i;
-	var x = document.getElementsByClassName("slide");
-	if (n > x.length) {
-		slideIndex = 1;
-	}    
-	if (n < 1) {
-		slideIndex = x.length;
-	}
-	for (i = 0; i < x.length; i++) {
-		x[i].style.display = "none";  
-	}
-	x[slideIndex-1].style.display = "block";
+    let i;
+    const x = document.getElementsByClassName('slide');
+    if (n > x.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i += 1) {
+        x[i].style.display = 'none';
+    }
+    x[slideIndex - 1].style.display = 'block';
 }
